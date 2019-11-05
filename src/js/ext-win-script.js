8fd8ec1ae646e205e1712a57edbd3cefa2fda5e0
David Corbett <ideetechnology@gmail.com>
2019-11-05 13:15:17 +0000

    fix bug on the js compile for production as the tasks weren't running in sequence

--- src/js/ext-win-script.js
+++ src/js/ext-win-script.js
@@ -3,7 +3,7 @@
  * Provides a script to append an icon to links that open in a new window / tab.
  * There are 3x different types of link / scenario where an icon should be
  * appended to a link in the `#main-content` area. Each is set out below.
- * @see documentation in src/scss/3_components/form/button/buttons.scss
+ * @see documentation in src/scss/3_components/content/icons.scss
  */
 /* eslint-disable */
 (function($, Drupal) {
@@ -24,10 +24,12 @@
     })
     .append('<span class="visually-hidden">(' + $extLinkText + ')</span><span class="ico ico-elink"></span>')
     .attr('title', $extLinkText)
-    .attr('target', '_blank');
+    .attr('target', '_blank')
+    .attr('rel', 'noopener noreferrer');
   // Internal links in content that open new windows (should be very rare)
   $("#main-content a[target='_blank']")
     .not("a.no-ext-icon, a:has(img), #main-content a[href*='http://'], #main-content a[href*='https://'], #main-content a[href^='//']")
     .append('<span class="visually-hidden">(' + $intLinkText + ')</span><span class="ico ico-elink"></span>')
-    .attr('title', $intLinkText);
+    .attr('title', $intLinkText)
+    .attr('rel', 'noopener noreferrer');
 })(jQuery, Drupal);
