2cc58ce84bce05b593ecb601463f1f030b788684
Neil Blair <neil.r.blair@gmail.com>
2021-08-11 16:21:01 +0100

    Ensure the hamburger toggle: - has a focus state - can be activated with the enter key - provides aria hints to indicate when the toggle is active or not.

--- src/js/main-menu-toggle.js
+++ src/js/main-menu-toggle.js
@@ -1,31 +1,39 @@
 /*
  * @file
  * Menu menu toggle (hamburger) behaviours.
+ *
+ * The hamburger menu uses an invisible checkbox placed over the top of the hamburger
+ * button.  The toggling of the checkbox and a bit of CSS serves to show and hide
+ * the main menu without the need for any JS.  However some behaviour needs added via
+ * JS to improve accessibility.
+ *
  * @see related html markup in templates/block/block--mainnavigation.html.twig
  * @see related css in src/scss/3_components/navigation/main-menu.scss
  */
+
 /* eslint-disable */
 (function($, Drupal) {
   Drupal.behaviors.nicsdruOriginsMainMenuToggle = {
     attach: function attach (context) {
-      /*
-      Make the hamburger checkbox toggle respond to the enter key.
-      The hamburger menu uses a checkbox and a bit of CSS to handle the display of the two states.
-      It works without any JS - users just need to check/uncheck the checkbox by touch, click, or
-      by pressing the space bar. But since it is styled to look like a button, sighted keyboard
-      users may try using the enter key to activate the button. So we must enable the toggling of
-      the checkbox by pressing enter.
-      */
       $('#menu-toggle', context).once('mainMenuToggle').each(function () {
-        $(this).keyup(function (event) {
-          var enterKeyCode = 13;
-          var spaceKeyCode = 32;
 
-          if (event.keyCode === enterKeyCode || event.keyCode === spaceKeyCode) {
+        // Checkboxes are normally toggled with the space bar. Enable toggling
+        // by pressing enter since it looks like a button (and the enter key is
+        // normally used to activate buttons).
+        $(this).keyup(function (event) {
+          var key = event.key || event.keyCode;
+          if (key === 'Enter' || key === 13) {
             this.checked = !this.checked;
           }
         });
+
+        // The checkbox has role="button" - toggle aria-pressed attribute when the
+        // the checkbox is toggled.
+        $(this).bind('click keyup', function () {
+           $(this).attr('aria-pressed', this.checked);
+        });
       });
+
     },
   };
 })(jQuery, Drupal);
