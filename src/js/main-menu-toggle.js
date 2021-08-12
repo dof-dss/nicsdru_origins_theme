fce11cf0ea3a5677296b9f762b3b3262748bf815
Neil Blair <neil.r.blair@gmail.com>
2021-08-12 14:43:31 +0100

    Replace the checkbox hack method for toggling the main nav menu - use button instead with JS to set aria attributes that make it accessible.

--- src/js/main-menu-toggle.js
+++ src/js/main-menu-toggle.js
@@ -2,11 +2,6 @@
  * @file
  * Menu menu toggle (hamburger) behaviours.
  *
- * The hamburger menu uses an invisible checkbox placed over the top of the hamburger
- * button.  The toggling of the checkbox and a bit of CSS serves to show and hide
- * the main menu without the need for any JS.  However some behaviour needs added via
- * JS to improve accessibility.
- *
  * @see related html markup in templates/block/block--mainnavigation.html.twig
  * @see related css in src/scss/3_components/navigation/main-menu.scss
  */
@@ -15,22 +10,18 @@
 (function($, Drupal) {
   Drupal.behaviors.nicsdruOriginsMainMenuToggle = {
     attach: function attach (context) {
-      $('#menu-toggle', context).once('mainMenuToggle').each(function () {
 
-        // Checkboxes are normally toggled with the space bar. Enable toggling
-        // by pressing enter since it looks like a button (and the enter key is
-        // normally used to activate buttons).
-        $(this).keyup(function (event) {
-          var key = event.key || event.keyCode;
-          if (key === 'Enter' || key === 13) {
-            this.checked = !this.checked;
-          }
-        });
+      $('.nav-main', context).once('mainMenuToggle').each(function () {
 
-        // Indicate the toggle has expanded the menu.
-        $(this).bind('click keyup', function () {
-           $(this).attr('aria-expanded', this.checked);
-        });
+        // Unhide the burger toggle and set aria attributes when the burger button is clicked.
+        var $burger = $('#nav-main-toggle');
+        $burger
+          .removeAttr('hidden')
+          .click(function() {
+            var active = $(this).attr('aria-expanded') === 'true' || false;
+            $(this).attr('aria-expanded', !active)
+            $(this).attr('aria-label', (active)? 'Open menu' : 'Close menu');
+          });
       });
 
     },
